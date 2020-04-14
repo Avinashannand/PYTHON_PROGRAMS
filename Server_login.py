37ca6adee8702342a9b7863528006a62156374d0
Avinash Kumar <resultspaceqa@sapient.com>
2020-04-14 05:59:52 +0530

    Required Changes and deleted unwnated files

--- Server_login.py
+++ Server_login.py
@@ -1,25 +1,27 @@
-#import socket
-#import libssh2
-#sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#sock.connect(('exmaple.com', 22))
-#session = libssh2.Session()
-#session.startup(sock)
-#session.userauth_password('john', '******')
-#channel = session.channel()
-#channel.execute('ls -l')
-#print(channel.read(1024))
-#shell = spur.SshShell(hostname="10.140.32.144", username="dev", password="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDahpVVNCBIeWfFi2smtMVxvNk3X20DIR/463IZ3edjIGY1y5rfiooSjD5ipc0H6hlQFrA02JLldrIIcRlGKsJ5Mq/JrFD7zSh+PBr9J1zPa3SWIjPnf+i7SH/+vw17xlMnGGJwGqcj+HkNrmpFJ9KrPEMgkdEswF+0RMPJFOxC91Oo6NWSGS0Qrk1EMZWSEb6+HcLvl/BY6xca1HqZffoidyRMJOzgSr8quB93O+y/EDxaVQGgGiIVkNkbpq5XAOI1+DUwPIegPim8j45ziT7z3LWB6X4dTG50SCQoOZ7JYQZxrvRC5VM2CIiDaB0HbTw7cDjPgF/XRLCGqC2GCn8N avinash@197NODNB")
-
 import spur
 import os
 
-shell = spur.SshShell(hostname="10.142.52.100", username="root", password="walletpaytm@123")
+shell = spur.SshShell(hostname="x.x.x.x", username="user", password="pass")
 
 #temp=os.popen(cmd).read()
 #list=temp.rstrip()
 
 result = shell.run(["ps", "-ef", "|", "grep paytm", "|", "awk", "'{print $2}'"])
-print(result.output)  #print the result
+print(result)  #print the result
+
+'''
+try:
+    shell = spur.SshShell("x.x.x.x",
+                      "user",
+                      "pass",
+                       missing_host_key=spur.ssh.MissingHostKey.accept)
+
+    with shell:
+        result = shell.run(["ls"])
+        print(result)
+except os.error:
+    print(error)
+    raise'''
 
 
 
